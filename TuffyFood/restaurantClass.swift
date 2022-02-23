688a9d0468f8d7964da9b0ea6e9300440e7f3baa
Nicholas Chung <Minsuhk@csu.fullerton.edu>
2022-02-22 16:11:06 -0800

    Nick's contribution

---
+++ TuffyFood/restaurantClass.swift
@@ -0,0 +1,37 @@
+//
+//  restaurantStruct.swift
+//  TuffyFood
+//
+//  Created by csuftitan on 2/22/22.
+//
+
+import Foundation
+
+class restaurant {
+    var title: String
+    var hours: String
+    var location: String
+    var priceRating: String
+    
+    init(name title: String, time hours: String, place location: String, price priceRating: String) {
+        self.title = title
+        self.hours = hours
+        self.location = location
+        self.priceRating = priceRating
+    }
+    func allInfo(){
+        print("\(title)'s hours are \(hours), and they can be found at \(location) with the price rating of \(priceRating)/3 $")
+    }
+    func titlePrint(){
+        print("\(title)")
+    }
+    func hoursPrint(){
+        print("\(hours)")
+    }
+    func locationPrint(){
+        print("\(location)")
+    }
+    func priceRatingPrint(){
+        print("\(priceRating)")
+    }
+}
---
+++ TuffyFood/restaurantObjects.swift
@@ -0,0 +1,53 @@
+//
+//  restaurantObjects.swift
+//  TuffyFood
+//
+//  Created by csuftitan on 2/22/22.
+//
+
+import Foundation
+
+var CarlsJr = restaurant(name:"Carl's Jr", time:"M→Th: 8:00am-5:00pm / F: 8:00am-2:00pm", place:"Outside Gordon Hall", price:"$$")
+
+var PollakLibaryStarbucks = restaurant(name:"Pollak Library Starbucks", time:"M→Th: 7:30am-9:00pm / F: 7:30am-4:00pm", place:"TSU", price:"$S$")
+
+var TSUStarbucks = restaurant(name:"TSU Starbucks", time:"M→Th: 8:00am-5:00pm / F: 8:00am-2:00pm", place:"TSU", price:"$$S")
+
+var CBEStarbucks = restaurant(name:"CBE Starbucks", time:"M→Th: 7:30am-7:00pm / F: 7:30am-4:00pm", place:"College of Business and Economics", price:"$$$")
+
+var HibachiSan = restaurant(name:"Hibachi San", time:"M→Th: 10:00am-5:00pm / F: 10:00am-2:00pm", place:"TSU", price:"$$")
+
+var PandaExpress = restaurant(name:"Panda Express", time:"M→Th: 9:00am-7:00pm / F: 9:00am-2:00pm", place:"TSU", price:"$$")
+
+var JuiceItUp = restaurant(name:"Juice It Up!", time:"M→Th: 8:00am-7:00pm / F: 8:00am-2:00pm", place:"TSU", price:"$$")
+
+//var TheFreshKitchen = restaurant(name:"The Fresh Kitchen", time:"Unknown", place:"TSU", price:"$$")
+
+var TogosSandwiches = restaurant(name:"Togo's Sandwiches", time:"M→Th: 10:00am-5:00pm / F: 10:00am-2:00pm", place:"TSU", price:"$$")
+
+var BajaFresh = restaurant(name:"Baja Fresh", time:"M→Th: 10:00am-5:00pm / F: 10:00am-2:00pm", place:"College of Business and Economics", price:"$")
+
+var Pieology = restaurant(name:"Pieology", time:"M→Th: 10:00am-7:00pm / F: 10:00am-2:00pm", place:"TSU", price:"$$")
+
+var AvantiMarkets = restaurant(name:"Avanti Markets", time:"M→Th: 7:00am-9:00pm / F: 7:00am-6:00pm", place:"College Park", price:"$")
+
+var Gastronome = restaurant(name:"Gastronome", time:"M→F: 7:00am-8:00pm / Sat→Sun: 9:00am-7:00pm", place:"Near CSUF housing and CS Building", price:"$$")
+
+//var LHExpress = restaurant(name:"LH Express", time:"Unknown", place:"Langsdorf Hall", price:"$$")
+
+var restaurantDictionary = ["Carl's Jr": CarlsJr,
+                            "Pollak Library Starbucks": PollakLibaryStarbucks,
+                            "TSU Starbucks": TSUStarbucks,
+                            "CBE Starbucks": CBEStarbucks,
+                            "Hibachi San": HibachiSan,
+                            "Panda Express": PandaExpress,
+                            "Juice It Up!": JuiceItUp,
+                            /*"The Fresh Kitchen": TheFreshKitchen,*/
+                            "Togo's Sandwiches": TogosSandwiches,
+                            "Baja Fresh": BajaFresh,
+                            "Pieology": Pieology,
+                            "Avanti Markets": AvantiMarkets,
+                            "Gastronome": Gastronome,
+                            /*"LH Express": LHExpress*/]
+
+
