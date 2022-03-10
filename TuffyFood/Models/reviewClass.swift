bbf8a0431ebbb715ab811d91ed428a6b2d16463f
Justin Lieng <70175007+JustinLieng@users.noreply.github.com>
2022-03-09 20:21:57 -0800

    Updating reviewClass.swift

--- TuffyFood/Models/reviewClass.swift
+++ TuffyFood/Models/reviewClass.swift
@@ -2,18 +2,24 @@ import Foundation
 
 class Reviews {
 
+    var name: String
     var comment: String
     var rating: Double
+    var anonymousCheck: Bool
+    var review: String
     
-    init( comment: String, rating: Double) {
+    init(name: String, comment: String, rating: Double, anonymousCheck: Bool) {
+        self.name = name
         self.comment = comment
         self.rating = rating
+        self.anonymousCheck = anonymousCheck
+        if anonymousCheck == true {
+            self.name = "Anonymous"
+        }
+        self.review = "\(name) gives a \(rating) rating and says \(comment)"
     }
     
     func displayReview() {
-        print("=============================\n")
-        print("Stars: \(rating)\n")
-        print("   > \(comment)\n")
-        print("=============================")
+        print(review)
     }
 }
