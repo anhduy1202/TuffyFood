2a8eba5da8b71737e1fc5c1ac27266b32988f50c
Daniel Truong <anhduy1202@gmail.com>
2022-03-10 17:01:49 -0800

    Retrieving data from Firebsae

--- TuffyFood/Controllers/HomeScreenViewController.swift
+++ TuffyFood/Controllers/HomeScreenViewController.swift
@@ -7,15 +7,28 @@
 
 import UIKit
 import FirebaseAuth
+import Firebase
 
 
 //This view controller responsible to display all places
 class HomeScreenViewController: UIViewController {
     var email: String? //User email
+    let ref = Database.database().reference().child("foodPlaces")
     override func viewDidLoad() {
         super.viewDidLoad()
         title = Constants.Storyboard.homeTitle
         navigationItem.hidesBackButton = true
+        
+        //Retrieve food places data from Firebase
+        ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            if let getData = snapshot.value as? [String:Any] {
+                    print(getData) //Array of object
+                /*To do:
+                    Parse the retrieved data to the restaurantClass
+                    Use the this data to create TableView to display each places
+                 */
+                }
+        })
     }
     @IBOutlet weak var profileButton: UITabBarItem!
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
