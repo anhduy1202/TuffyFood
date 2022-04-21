1cff9d1c9329568e89ed499a32ab59c50bbf2129
Daniel Truong <aduy1122@gmail.com>
2022-04-20 17:13:05 -0700

    Adding stars for the rrating

--- TuffyFood/Controllers/PlaceDetailViewController.swift
+++ TuffyFood/Controllers/PlaceDetailViewController.swift
@@ -8,11 +8,39 @@
 import UIKit
 
 class PlaceDetailViewController: UIViewController {
+    @IBOutlet weak var placeLabel: UILabel!
     var placeData: restaurant?
     var placeImg: String?
+    var rating: Int?
+    @IBOutlet weak var star1: UILabel!
+    @IBOutlet weak var star2: UILabel!
+    @IBOutlet weak var star3: UILabel!
+    @IBOutlet weak var star4: UILabel!
+    @IBOutlet weak var star5: UILabel!
     @IBOutlet weak var coverPhoto: UIImageView!
+    var starArray = [UILabel]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        star1.isHidden = true
+        star2.isHidden = true
+        star3.isHidden = true
+        star4.isHidden = true
+        star5.isHidden = true
+        starArray.append(star1)
+        starArray.append(star2)
+        starArray.append(star3)
+        starArray.append(star4)
+        starArray.append(star5)
+        self.rating = placeData?.rating
+
+        for (index, star) in  starArray.enumerated() {
+            if (index == rating) {
+                break;
+            }
+            star.isHidden = false
+        }
+        
+        placeLabel.text = placeData?.title
         self.title = placeData?.title
         coverPhoto.loadFrom(URLAddress: placeData?.image ?? "")
     }
