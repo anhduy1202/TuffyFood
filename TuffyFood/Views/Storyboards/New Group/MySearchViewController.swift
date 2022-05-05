6cd92a074b5653ae13d3d7ce24cb323be80fed81
Nicholas Chung <70074654+Minsuhk@users.noreply.github.com>
2022-05-05 10:38:01 -0700

    finish with cleanup hexed

--- TuffyFood/Views/Storyboards/New Group/MySearchViewController.swift
+++ TuffyFood/Views/Storyboards/New Group/MySearchViewController.swift
@@ -10,26 +10,7 @@ import Foundation
 var newResList: [myTestRestaurant] = [myTestRestaurant(name:"Panda", desc:"asian food"), myTestRestaurant(name:"Pieology", desc:"american food"), myTestRestaurant(name:"Sandwich", desc:"american food"), myTestRestaurant(name:"Ramen", desc:"asian food")]
 
 class SearchViewController: UIViewController/*, UITableViewDataSource*/ {
-    /*
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-    }
-    func numberOfSections(in tableView: UITableView) -> Int{
-        return 1
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)-> Int{
-        return newResList.count
-    }
-    func tableView(_ tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath)->UITableViewCell{
-        var cell = UITableViewCell()
-        
-        let(res) = newResList[indexPath.row]
-        
-        cell.textLabel?.text = res
-        return cell
-    }
-    */
-    override func viewDidLoad() {
+       override func viewDidLoad() {
         super.viewDidLoad()
         //tableView.delegate = self
         //tableView.dataSource = self
@@ -50,16 +31,11 @@ class myTestRestaurant {
 }
 class restaurantManager {
     var presence: Bool?
-    //var restaurantList: [String: String] = ["Panda Express": "Open from 10am to 2pm ; Rated 4/5 ; Asian food", "Carl's Jr.": "Open from 8am to 4pm ; Rated 3/5 ; American food"]
     
     var tempArray: [myTestRestaurant] = []
     
-//    var newResList: [myTestRestaurant] = [myTestRestaurant(name:"Panda", desc:"asian food"), myTestRestaurant(name:"Pieology", desc:"american food"), myTestRestaurant(name:"Sandwich", desc:"american food"), myTestRestaurant(name:"Ramen", desc:"asian food")]
-    
     func addToList(newName: String, description: String){
-        //newResList.append(myTestRestaurant(name: newName, desc: description))
         newResList.append(myTestRestaurant(name: newName, desc: description))
-        //restaurantList.append(newResList)
     }
     
     @IBOutlet var RestaurantPrintTextBox: UITextField!
@@ -69,7 +45,6 @@ class restaurantManager {
     @IBOutlet var PrintArray: UITableView!
     
     func searchList(nameToFind: String)->[myTestRestaurant]{
-        //var listSize: Int  = restaurantList.count
         
         for i in newResList {
             if nameToFind == i.name{
