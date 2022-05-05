0b6adb15efaccb9c323fc1e7bf7ebc61d7cb0e2b
JustinLieng <jlieng@csu.fullerton.edu>
2022-05-05 08:30:43 -0700

    check in 5 Justin

--- TuffyFood/Models/User.swift
+++ TuffyFood/Models/User.swift
@@ -16,7 +16,12 @@ struct User {
     
     mutating func addFavorites (name: String, place: String, price: String, image: String, rating: String) {
         let restaurant = restaurant(name: name, time: "M→Th: 9:00am-7:00pm / F: 9:00am-2:00pm", place: place, price: price, image: image, rating: rating)
-        
+        for (index, rest) in favorites.enumerated() {
+            if restaurant.title == rest.title {
+                favorites.remove(at: index)
+                return
+            }
+        }
         favorites.append(restaurant)
     }
 }
